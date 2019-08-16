2b8f66e84fca75709a1a133322d1d490b204cc10
Léu Almeida <leo@webid.net.br>
2019-08-16 14:07:32 -0300

    Update header.jsx

--- frontend/src/common/template/header.jsx
+++ frontend/src/common/template/header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default props =>(
     <header className='main-header'>
         <a href='/#/' className='logo'>
-            <span className='logo-mini'><b>My</b>M</span>
+            <span className='logo-mini'><b>Léu</b></span>
             <span className='logo-lg'>
                 <i className='fa fa-money'></i>
-                <b> My</b> Money
+                <b> Léu</b> Almeida
             </span>
         </a>
         <nav className='navbar navbar-static-top'>
