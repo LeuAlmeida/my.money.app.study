d808a83324ebbe6b0de4faf58508234ffbcc87ac
Léu Almeida <leo@webid.net.br>
2019-08-16 14:06:24 -0300

    Create footer.jsx

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2019
+            <a href="http://webid.net.br" target='_blank'> WebID</a>
+        </strong>
+    </footer>
+)
