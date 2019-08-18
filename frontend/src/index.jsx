d2aed301ef1cd04fa4970a25052a483878743469
Léu Almeida <leo@webid.net.br>
2019-08-18 18:41:15 -0300

    Update index.jsx with Redux Multi and Thunk

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,12 +4,14 @@ import { applyMiddleware, createStore } from 'redux' // Importa o Create Store q
 import { Provider } from 'react-redux' // Importa a integração do React com o Redux
 
 import promise from 'redux-promise'
+import multi from 'redux-multi' // Middleware que permite dispatch através de array de actions
+import thunk from 'redux-thunk'
 
 import App from './main/app' // Importa a aplicação 
 import reducers from './main/reducers' //Importa os Reducers criados
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools) // Chama todos os Reducers para envolver a aplicação em <App /> com o <Provider> puxando essa constante
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools) // Chama todos os Reducers para envolver a aplicação em <App /> com o <Provider> puxando essa constante
 
 ReactDOM.render(
 // Renderiza a aplicação na DOM
