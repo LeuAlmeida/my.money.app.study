b6891d18119b8e6698dde65047773272f8de1d8b
Léu Almeida <leo@webid.net.br>
2019-08-18 01:16:39 -0300

    Update Reducers

--- frontend/src/billingCycle/billingCycleReducer.js
+++ frontend/src/billingCycle/billingCycleReducer.js
@@ -2,6 +2,9 @@ const INITIAL_STATE = {list: []}
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        
+        case 'BILLING_CYCLES_FETCHED':
+            return { ...state, list: action.payload.data}
+        default:
+            return state
     }
 }
