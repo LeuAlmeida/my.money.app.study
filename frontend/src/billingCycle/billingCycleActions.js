dccdb4d9ad6c6d904ba043787fce1f40c9d602c8
Léu Almeida <leo@webid.net.br>
2019-08-18 18:42:30 -0300

    Update billingCycleActions.js

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm, initialize } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
+
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,14 +14,27 @@ export function getList() {
 }
 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação realizada com sucesso.')
+            dispatch([
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList', 'tabCreate')
+            ])
         })
         .catch(e => {
             e.response.data.errors.forEach(error => toastr.error('Erro', error))
         })
-    return {
-        type: 'TEMP'
     }
+}
+
+export function showUpdate(billingCycle) {
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', billingCycle)
+    ]
 }
