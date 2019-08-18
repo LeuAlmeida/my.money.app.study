a8b1ba9529777091976fe972652ff9055f068de6
Léu Almeida <leo@webid.net.br>
2019-08-17 21:25:34 -0300

    Update App index

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './main/app'
+import React from 'react' // Importa o React
+import ReactDOM from 'react-dom' // Importa o React para visualização no browser
+import { applyMiddleware, createStore } from 'redux' // Importa o Create Store que pertence ao Redux
+import { Provider } from 'react-redux' // Importa a integração do React com o Redux
 
-ReactDOM.render(<App />, document.getElementById('app'))
+import promise from 'redux-promise'
+
+import App from './main/app' // Importa a aplicação 
+import reducers from './main/reducers' //Importa os Reducers criados
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devTools) // Chama todos os Reducers para envolver a aplicação em <App /> com o <Provider> puxando essa constante
+
+ReactDOM.render(
+// Renderiza a aplicação na DOM
+    <Provider store={store}>
+        <App />
+    </Provider>
+, document.getElementById('app'))
