d0a9a7082c8db9d7ca3f7d378afb2c0c04da282b
Léu Almeida <leo@webid.net.br>
2019-08-18 12:15:16 -0300

    Update billignCycleList.jsx

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -7,7 +7,17 @@ class BillingCycleList extends Component {
     
     componentWillMount() {
         this.props.getList()
-        console.log(this.props.list)
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
     }
 
     render() {
@@ -22,7 +32,7 @@ class BillingCycleList extends Component {
                         </tr>
                     </thead>
                     <tbody>
-
+                        {this.renderRows()}
                     </tbody>
                 </table>
             </div>
