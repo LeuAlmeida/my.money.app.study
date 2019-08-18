e678703beeef327b6f23ea7ce86fd6ea56baff5b
Léu Almeida <leo@webid.net.br>
2019-08-18 18:42:44 -0300

    Update billingCycle.jsx

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -41,7 +41,9 @@ class BillingCycle extends Component {
                                 <TabContent id='tabCreate'>
                                     <Form onSubmit={this.props.create} />
                                 </TabContent>
-                                <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                                <TabContent id='tabUpdate'>
+                                    <Form />
+                                </TabContent>
                                 <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
                         </TabsContent>
                     </Tabs>
