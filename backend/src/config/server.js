ad302d093a48eead98716785fc983c788eb9e35e
Léu Almeida <leo@webid.net.br>
2019-08-18 13:19:29 -0300

    Update server.js

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -6,13 +6,13 @@ const server = express()
 const allowCors = require('./cors')
 const queryParser = require('express-query-int')
 
-server.use(bodyParser.urlencoded({ extended:true }))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 server.use(queryParser())
 
 server.listen(port, function() {
-    console.log(`Backend is running on port ${port}.`)
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
