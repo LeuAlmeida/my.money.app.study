d01e323f2898102c235b92b202483c83daa88aa7
Léu Almeida <leo@webid.net.br>
2019-08-18 13:18:55 -0300

    Update errorHandler.js

--- backend/src/api/common/errorHandler.js
+++ backend/src/api/common/errorHandler.js
@@ -3,10 +3,10 @@ const _ = require('lodash')
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
 
-    if(bundle.erros) {
-        const errors = parserErrors(bundle.errors)
+    if(bundle.errors) {
+        const errors = parseErrors(bundle.errors)
         res.status(500).json({errors})
-    }else{
+    } else {
         next()
     }
 }
@@ -14,5 +14,5 @@ module.exports = (req, res, next) => {
 const parseErrors = (nodeRestfulErrors) => {
     const errors = []
     _.forIn(nodeRestfulErrors, error => errors.push(error.message))
-    return errors
+    return errors 
 }
