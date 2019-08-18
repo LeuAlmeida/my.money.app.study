013c34c59e2a15bd180bffb3a48b660ef063c040
Léu Almeida <leo@webid.net.br>
2019-08-18 12:55:20 -0300

    Update reducers.js

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
@@ -6,7 +8,8 @@ import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
-    billingCycle: BillingCycleReducer
+    billingCycle: BillingCycleReducer,
+    form: formReducer
 })
 
 export default rootReducer
