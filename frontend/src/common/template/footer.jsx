40612765c0279a4479bcab39fb2d3e24f91ed50e
Léu Almeida <leo@webid.net.br>
2019-08-18 21:30:06 -0300

    Update footer.jsx

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 export default props => (
     <footer className='main-footer'>
         <strong>
-            Copyright &copy; 2019
-            <a href="http://webid.net.br" target='_blank'> WebID</a>
+            Copyright &copy; {new Date().getFullYear()}
+            <a href="http://webid.net.br" target='_blank'> WebID</a> | All rights reserved.
+        </strong>
+        <strong className='text-right'>
+        <i className='fab fa-github'></i>
         </strong>
     </footer>
 )
