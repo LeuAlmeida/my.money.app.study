ceb8c4f6edba3c5249cb688a5e342923d2eaa14e
Léu Almeida <leo@webid.net.br>
2019-08-20 14:43:38 -0300

    Update routes.jsx

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -3,11 +3,13 @@ import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
+import Clients from '../clients/clients'
 
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard} />
         <Route path ='/billingCycles' component={BillingCycle} />
+        <Route path ='/clients' component={Clients} />
         <Redirect from='*' to='/' />
     </Router>
 )
