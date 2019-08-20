47152f7da0efff9bd307eddf4d4d3a27b91eee8b
Léu Almeida <leo@webid.net.br>
2019-08-20 14:43:21 -0300

    Update reducers.js

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -5,13 +5,15 @@ import { reducer as toastrReducer } from 'react-redux-toastr'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import ClientsReducer from '../clients/clientsReducer'
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
     form: formReducer,
-    toastr: toastrReducer
+    toastr: toastrReducer,
+    clients: ClientsReducer
 })
 
 export default rootReducer
