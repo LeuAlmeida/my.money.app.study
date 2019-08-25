e5a6861b92e8313af44ba4e1010630cec44ab3f9
Léu Almeida <leo@webid.net.br>
2019-08-25 20:20:23 -0300

    Update routes.js

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,16 +1,33 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
 
-    // Definir URL base para todas as rotas 
-    const router = express.Router()
-    server.use('/api', router)
+    ////////////////////////////////////
+    // Rotas protegidas por Token JWT //
+    ////////////////////////////////////
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
+    protectedApi.use(auth)
+    
     // Rotas de Ciclo de Pagamento 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
 
     // Rotas de Clients 
     const Clients = require('../api/clients/clientsService')
-    Clients.register(router, '/clients')
+    Clients.register(protectedApi, '/clients')
+
+    ///////////////////
+    // Rotas abertas //
+    ///////////////////
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
 }
