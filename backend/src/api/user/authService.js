13553344d917855bae49f367bf9e021f074ccf27
Léu Almeida <leo@webid.net.br>
2019-08-26 12:59:53 -0300

    Update authService.js

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -16,6 +16,7 @@ const sendErrorsFromDB = (res, dbErrors) => {
 const login = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
+
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
@@ -33,6 +34,7 @@ const login = (req, res, next) => {
 
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
+
     jwt.verify(token, env.authSecret, function(err, decoded) {
         return res.status(200).send({ valid: !err })
     })
@@ -43,21 +45,25 @@ const signup = (req, res, next) => {
     const email = req.body.email || ''
     const password = req.body.password || ''
     const confirmPassword = req.body.confirm_password || ''
+
     if (!email.match(emailRegex)) {
-        return res.status(400).send({ errors: ['O e-mail informa está inválido'] })
+        return res.status(400).send({ errors: ['O e-mail informado está inválido'] })
     }
+
     if (!password.match(passwordRegex)) {
         return res.status(400).send({
             errors: [
-                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$%) e tamanho entre 6-20."
+                "Senha precisar ter: uma letra maiúscula, uma letra minúscula, um número, uma caractere especial(@#$ %) e tamanho entre 6-20."
             ]
         })
     }
+
     const salt = bcrypt.genSaltSync()
     const passwordHash = bcrypt.hashSync(password, salt)
     if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
         return res.status(400).send({ errors: ['Senhas não conferem.'] })
     }
+
     User.findOne({ email }, (err, user) => {
         if (err) {
             return sendErrorsFromDB(res, err)
