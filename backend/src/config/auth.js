8a2c86ccd24a20ae1a9876717337e85323f12219
Léu Almeida <leo@webid.net.br>
2019-08-25 23:45:49 -0300

    Update auth.js

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -6,19 +6,18 @@ module.exports = (req, res, next) => {
     if (req.method === 'OPTIONS') {
         next()
     } else {
-        const token = req.body.token || req.query.token || req.headers['authorization']
-
+        const token = req.body.token || req.query.token ||
+            req.headers['authorization']
         if (!token) {
             return res.status(403).send({ errors: ['No token provided.'] })
         }
-
-        jwt.verify(token, env.authSecret, function (err, decoded) {
+        jwt.verify(token, env.authSecret, function(err, decoded) {
             if (err) {
                 return res.status(403).send({
                     errors: ['Failed to authenticate token.']
                 })
             } else {
-                // req.decoded = decoded
+                req.decoded = decoded
                 next()
             }
         })
