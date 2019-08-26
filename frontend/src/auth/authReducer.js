96eb4dba476bd2ba7d79b4e92bc903161e721b7f
Léu Almeida <leo@webid.net.br>
2019-08-25 23:46:41 -0300

    Update authReducer.js

--- frontend/src/auth/authReducer.js
+++ frontend/src/auth/authReducer.js
@@ -8,14 +8,14 @@ export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case 'TOKEN_VALIDATED':
             if (action.payload) {
-                return { ...state, validToken: true }
+                return {...state, validToken: true }
             } else {
                 localStorage.removeItem(userKey)
-                return { ...state, validToken: false, user: null }
+                return {...state, validToken: false, user: null }
             }
         case 'USER_FETCHED':
             localStorage.setItem(userKey, JSON.stringify(action.payload))
-            return { ...state, user: action.payload, validToken: true }
+            return {...state, user: action.payload, validToken: true }
         default:
             return state
     }
