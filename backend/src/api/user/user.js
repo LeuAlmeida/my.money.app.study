051f714fabcf6b82444cfe0359c3754482890e85
Léu Almeida <leo@webid.net.br>
2019-08-25 23:45:35 -0300

    Update user.js

--- backend/src/api/user/user.js
+++ backend/src/api/user/user.js
@@ -2,7 +2,7 @@ const restful = require('node-restful')
 const mongoose = restful.mongoose
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true},
+    name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, min: 6, max: 12, required: true }
 })
