9384e263596b44db6e5b0b4e711c9f66b2c3ae9b
Léu Almeida <leo@webid.net.br>
2019-08-26 13:00:01 -0300

    Update user.js

--- backend/src/api/user/user.js
+++ backend/src/api/user/user.js
@@ -4,7 +4,7 @@ const mongoose = restful.mongoose
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, min: 6, max: 12, required: true }
+    password: { type: String, min: 6, max: 20, required: true }
 })
 
 module.exports = restful.model('User', userSchema)
