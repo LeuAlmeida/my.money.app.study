ab3d684f7c2c1815af3c49887f5a2e665eb6964a
Léu Almeida <leo@webid.net.br>
2019-08-25 23:46:47 -0300

    Update inputAuth.jsx

--- frontend/src/common/form/inputAuth.jsx
+++ frontend/src/common/form/inputAuth.jsx
@@ -4,8 +4,13 @@ import If from '../operator/if'
 export default props => (
     <If test={!props.hide}>
         <div className="form-group has-feedback">
-            <input {...props.input} className='form-control' placeholder={props.placeholder} readOnly={props.readOnly} type={props.type} />
-            <span className={`glyphicon glyphicon-${props.icon} form-control-feedback`}></span>
+            <input {...props.input}
+                className='form-control'
+                placeholder={props.placeholder}
+                readOnly={props.readOnly}
+                type={props.type} />
+            <span className={`glyphicon glyphicon-${props.icon}
+  form-control-feedback`}></span>
         </div>
     </If>
 )
