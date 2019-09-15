04e809b13f7b4bb6788a82334cad8c55fa95ae4d
Léu Almeida <leo@webid.net.br>
2019-09-14 21:48:52 -0300

    Update app.jsx

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
-import Routes from './routes'
+// import Routes from './routes'
 import Messages from '../common/msg/messages'
 
 export default props => (
@@ -11,7 +11,8 @@ export default props => (
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            <Routes />
+            {/* <Routes /> */}
+            {props.children}
         </div>
         <Footer />
         <Messages />
