5d90f5ee211067a328e96b02c6180a9a31678c4a
Léu Almeida <leo@webid.net.br>
2019-09-14 21:49:12 -0300

    Update auth.jsx

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -1,5 +1,4 @@
 import './auth.css'
-
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { connect } from 'react-redux'
@@ -8,21 +7,26 @@ import { bindActionCreators } from 'redux'
 import { login, signup } from './authActions'
 import Row from '../common/layout/row'
 import Grid from '../common/layout/grid'
+// import If from '../common/operator/if'
 import Messages from '../common/msg/messages'
 import Input from '../common/form/inputAuth'
 
 class Auth extends Component {
+
     constructor(props) {
         super(props)
         this.state = { loginMode: true }
     }
+
     changeMode() {
         this.setState({ loginMode: !this.state.loginMode })
     }
+
     onSubmit(values) {
         const { login, signup } = this.props
         this.state.loginMode ? login(values) : signup(values)
     }
+
     render() {
         const { loginMode } = this.state
         const { handleSubmit } = this.props
@@ -30,7 +34,7 @@ class Auth extends Component {
             <div className="login-box">
                 <div className="login-logo"><b> My</b> Money</div>
                 <div className="login-box-body">
-                    <p className="login-box-msg">Bem-vindo!</p>
+                    <p className="login-box-msg">Bem vindo!</p>
                     <form onSubmit={handleSubmit(v => this.onSubmit(v))}>
                         <Field component={Input} type="input" name="name"
                             placeholder="Nome" icon='user' hide={loginMode} />
@@ -60,6 +64,7 @@ class Auth extends Component {
         )
     }
 }
+
 Auth = reduxForm({ form: 'authForm' })(Auth)
 const mapDispatchToProps = dispatch => bindActionCreators({ login, signup },
     dispatch)
