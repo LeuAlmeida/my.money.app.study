1e74d93ad00be479496b801e411a4f6801cf0e54
Léu Almeida <leo@webid.net.br>
2019-09-14 21:49:57 -0300

    Update routes.jsx

--- frontend/src/main/routes.jsx
+++ frontend/src/main/routes.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 
-import AuthOrApp from './authOrApp'
+// import AuthOrApp from './authOrApp'
+import App from './app'
 import Dashboard from '../dashboard/dashboard'
 import BillingCycle from '../billingCycle/billingCycle'
 import Clients from '../clients/clients'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={AuthOrApp}>
+        <Route path='/' component={App}> {/*Anterior: component=AuthOrApp*/}
             <IndexRoute component={Dashboard} />
             <Route path ='billingCycles' component={BillingCycle} />
             <Route path ='clients' component={Clients} />
